8f25fcb2fd254dd7957e89f9d068491959f643ba
santerj <45292222+santerj@users.noreply.github.com>
2021-06-13 22:56:46 +0300

    Create login-police.sh

---
+++ login-police.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+host=$(hostname --fqdn)
+date=$(date +"%a %F %T %Z")
+
+if [ "$PAM_TYPE" != "close_session" ]; then
+    notification="New login: $PAM_USER from $PAM_RHOST on $host at $date"
+    /usr/local/bin/notifier.sh "$notification"
+fi
