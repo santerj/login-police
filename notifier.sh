c31e2ccc1f51fb061ec620b5841e424edb5bd0e7
santerj <45292222+santerj@users.noreply.github.com>
2021-06-13 23:04:33 +0300

    Delete notifier.sh

--- notifier.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TOKEN=""   # my Telegram bot token
-ID=""      # my Telegram chat id
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-MESSAGE=$@
-
-curl -s -X POST $URL -d chat_id=$ID -d text="$@" > /dev/null 2>&1
