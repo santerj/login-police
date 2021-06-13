519c3510216a79308a37089522ae63f202f40929
santerj <45292222+santerj@users.noreply.github.com>
2021-06-13 23:04:18 +0300

    merge files into one

--- login-police.sh
+++ login-police.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-host=$(hostname --fqdn)
-date=$(date +"%a %F %T %Z")
+TOKEN=""   # my Telegram bot token
+ID=""      # my Telegram chat id
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+
+HOST=$(hostname --fqdn)
+DATE=$(date +"%a %F %T %Z")
 
 if [ "$PAM_TYPE" != "close_session" ]; then
-    notification="New login: $PAM_USER from $PAM_RHOST on $host at $date"
-    /usr/local/sbin/notifier.sh "$notification"
+    notification="New login: $PAM_USER from $PAM_RHOST on $HOST at $DATE"
+    curl -s -X POST $URL -d chat_id=$ID -d text="$notification" > /dev/null 2>&1
 fi
