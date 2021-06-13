6d34903fe5a7f41f8b35f705231551ff7be2f60b
santerj <45292222+santerj@users.noreply.github.com>
2021-06-13 22:58:51 +0300

    bin -> sbin

--- login-police.sh
+++ login-police.sh
@@ -5,5 +5,5 @@ date=$(date +"%a %F %T %Z")
 
 if [ "$PAM_TYPE" != "close_session" ]; then
     notification="New login: $PAM_USER from $PAM_RHOST on $host at $date"
-    /usr/local/bin/notifier.sh "$notification"
+    /usr/local/sbin/notifier.sh "$notification"
 fi
